cbab885059298ca45e1751b36a507e441682b21b
Antonin Décimo <antonin@tarides.com>
2021-06-04 13:43:03 +0200

    patch antonindecimo

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ extract)
   git clone --recursive --depth=1 --branch=windows https://github.com/MisterDA/ocluster.git
 
   cd ocluster || exit
-  patch -Np1 <<EOF
+  patch -Np1 << 'EOF'
 diff --git a/bin/winsvc_wrapper.winsvc.ml b/bin/winsvc_wrapper.winsvc.ml
 index c4055ec..2802854 100644
 --- a/bin/winsvc_wrapper.winsvc.ml
@@ -59,17 +59,55 @@ EOF
   ocaml-env exec -- dune install --prefix=install --relocatable --root=.
 
   cd ../docker-base-images || exit
+  patch -Np1 << 'EOF'
+diff --git a/src/base_images.ml b/src/base_images.ml
+index 32a64ea..e951894 100644
+--- a/src/base_images.ml
++++ b/src/base_images.ml
+@@ -9,7 +9,7 @@ let () = Prometheus_unix.Logging.init ()
+
+ (* A low-security Docker Hub user used to push images to the staging area.
+    Low-security because we never rely on the tags in this repository, just the hashes. *)
+-let staging_user = "ocurrentbuilder"
++let staging_user = "antonindecimo"
+
+ let read_first_line path =
+   let ch = open_in path in
+diff --git a/src/conf.ml b/src/conf.ml
+index e197878..10de546 100644
+--- a/src/conf.ml
++++ b/src/conf.ml
+@@ -1,7 +1,7 @@
+ (* For staging arch-specific builds before creating the manifest. *)
+-let staging_repo = "ocurrent/opam-staging"
++let staging_repo = "antonindecimo/opam-windows"
+
+-let public_repo = "ocaml/opam"
++let public_repo = "antonindecimo/opam-windows"
+
+ let password_path =
+   let open Fpath in
+@@ -22,7 +22,7 @@ let auth =
+     let len = in_channel_length ch in
+     let password = really_input_string ch len |> String.trim in
+     close_in ch;
+-    Some ("ocurrent", password)
++    Some ("antonindecimo", password)
+   ) else (
+     Fmt.pr "Password file %S not found; images will not be pushed to hub@." password_path;
+     None
+@@ -56,7 +56,7 @@ let switches ~arch ~distro =
+ (* We can't get the active distros directly, but assume x86_64 is a superset of everything else. *)
+ let distros = Dockerfile_distro.(active_distros `X86_64 |> List.filter (fun d ->
+   match os_family_of_distro d with
+-  | `Linux | `Windows -> true
++  | `Linux -> false | `Windows -> true
+   | _ -> false))
+
+ let arches_for ~distro = Dockerfile_distro.distro_arches Ocaml_version.Releases.latest distro
+EOF
+
   mkdir -p install
-  cd docker-base-images || exit
-  sed -i'' \
-      -e 's|ocurrent/opam-staging|antonindecimo/opam-windows|g' \
-      -e 's|ocaml/opam|antonindecimo/opam-windows|g' \
-      -e 's|\"ocurrent\"|\"antonindecimo\"|g' \
-      -e 's/  | `Linux | `Windows -> true/  | `Windows -> true/g' \
-      src/conf.ml
-  sed -i'' \
-      -e 's|\"ocurrentbuilder\"|\"antonindecimo\"|g' \
-      src/base_images.ml
   ocaml-env exec -- dune build @install --profile=$PROFILE
   ocaml-env exec -- dune install --prefix=install --relocatable
 
